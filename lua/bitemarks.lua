c3f64d2b170b8edabbe30e6c6216f010a4cf0095
Elias Rusch <eliasrusch@atmedia.net>
2024-06-06 10:38:14 +0000

    changed mark position

--- lua/bitemarks.lua
+++ lua/bitemarks.lua
@@ -18,7 +18,7 @@ function M.mark(mark)
   local buffer_id = vim.api.nvim_get_current_buf()
   local win_id = vim.api.nvim_get_current_win()
   local line_number = vim.api.nvim_win_get_cursor(win_id)[1] - 1
-  local col = 0
+  local col = 5
   vim.api.nvim_buf_del_extmark(buffer_id, namespace_id, id)
   vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, line_number, col, {
     id = id,
