e216dd84ac1dd1d8fdfc357d71625c8e00d4ddf7
Elias Rusch <eliasrusch@atmedia.net>
2024-06-06 10:56:36 +0000

    addded new mark

--- lua/bitemarks.lua
+++ lua/bitemarks.lua
@@ -13,8 +13,9 @@ function M.run_autocommands()
 end
 
 function M.mark(mark)
-  local id1 = string.byte(mark)
-  local id2 = string.byte(mark)
+  local id1 = string.byte(mark .. '*')
+  local id2 = string.byte(mark .. '\'')
+  local id3 = string.byte(mark .. '')
 
   local buffer_id = vim.api.nvim_get_current_buf()
   local win_id = vim.api.nvim_get_current_win()
@@ -24,15 +25,21 @@ function M.mark(mark)
   vim.api.nvim_buf_del_extmark(buffer_id, namespace_id, id1)
   vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, line_number, 0, {
     id = id1,
-    virt_text = {{ "*" , "BiteMarks"}},
-    virt_text_pos = "inline",
+    virt_text = { { "*", "BiteMarks" } },
+    virt_text_win_col = 0,
   })
 
   vim.api.nvim_buf_del_extmark(buffer_id, namespace_id, id2)
   vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, line_number, col, {
     id = id2,
     virt_text = { { "'" .. mark, "BiteMarks" } },
-    virt_text_pos = "right_align"
+    virt_text_pos = "right_align",
+  })
+
+  vim.api.nvim_buf_del_extmark(buffer_id, namespace_id, id3)
+  vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, line_number, col, {
+    id = id3,
+    virt_text = { { "     '" .. mark, "BiteMarks" } },
   })
 
   vim.api.nvim_feedkeys("m" .. mark, "n", true)
