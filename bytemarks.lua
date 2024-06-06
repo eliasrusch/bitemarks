f01e10d7b945320aacd113c6b53e305b4854214b
Elias Rusch <eliasrusch@atmedia.net>
2024-06-06 00:38:12 +0000

    Initial commit

---
+++ bytemarks.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.init_highlight_group()
+  vim.api.nvim_command("highlight default ByteMarks guifg=#cba6f7")
+  namespace_id = vim.api.nvim_create_namespace("ByteMarks")
+end
+
+function M.run_autocommands()
+  vim.api.nvim_command("augroup ByteMarks")
+  vim.api.nvim_command("autocmd!")
+  vim.api.nvim_command("autocmd ColorScheme * lua require'bytemarks'.init_highlight_group()")
+  vim.api.nvim_command("augroup end")
+end
+
+function M.mark(mark)
+  local id = string.byte(mark)
+
+  local buffer_id = vim.api.nvim_get_current_buf()
+  local win_id = vim.api.nvim_get_current_win()
+  local line_number = vim.api.nvim_win_get_cursor(win_id)[1] - 1
+  local col = 0
+  vim.api.nvim_buf_del_extmark(buffer_id, namespace_id, id)
+  vim.api.nvim_buf_set_extmark(buffer_id, namespace_id, line_number, col, {
+    id = id,
+    virt_text = { { "'" .. mark, "ByteMarks" } },
+    virt_text_pos = "right_align",
+    virt_text_win_col = -6,
+  })
+
+  vim.api.nvim_feedkeys("m" .. mark, "n", true)
+end
+
+function M.setup()
+  require("bytemarks").init_highlight_group()
+  require("bytemarks").run_autocommands()
+  vim.keymap.set("n", "m", "<cmd>lua require'bytemarks'.mark(vim.fn.getcharstr())<CR>")
+end
+
+return M
